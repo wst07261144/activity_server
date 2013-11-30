b94d06d6ef189246ff2bf5206e13d2841332e88f
wst07261144 <wst07261144@gmail.com>
2013-11-30 17:07:40 +0800

    登录功能实现

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,6 +1,21 @@
 class UserController < ApplicationController
+
   def login
-    @title='welcome to beijing'
+  end
+
+  def handle_login
+    p params
+    username = params[:user][:name]
+    password = params[:user][:password]
+    @user = User.find_by_name_and_password username,password
+    if !@user.nil?
+      redirect_to '/user/login_success'
+      #redirect_to  user_login_success_path
+      session[:user]   =  @user
+    else
+      @error = '用户名或者密码不正确'
+      render 'login'
+    end
   end
 
   def logout
@@ -10,8 +25,14 @@ class UserController < ApplicationController
   end
 
   def login_success
+    @user = session[:user]
   end
 
   def reset_key
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name,  :password  )
   end
 end
