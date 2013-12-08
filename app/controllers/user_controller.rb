c7efee1ceaf67343a8b3d3a1b80aa7ffcde474c3
wst07261144 <wst07261144@gmail.com>
2013-12-08 20:12:17 +0800

    改变主页路由配置

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,15 +1,39 @@
 class UserController < ApplicationController
-  # include UserHelper
-  def login
 
-  end
-  def logout
 
-  end
   def register
   end
   def add_account
   end
+
+  def logout
+    session[:login] = false
+    redirect_to "/user/login"
+  end
+
+  def login_success
+    @user = session[:user]
+    session[:login] = true
+  end
+
+  def handle_login
+    username = params[:user][:name]
+    password = params[:user][:password]
+    @user = User.find_by_name_and_password username, password
+    if !@user.nil?
+      if @user.name!="admin"
+        redirect_to '/user/login_success' #redirect_to  user_login_success_path
+        session[:user] = @user
+      else
+        redirect_to '/user/manage_index'
+      end
+    else
+      flash.now[:notice]= '用户名或者密码不正确'
+      render 'login'
+    end
+  end
+
+
   def admin_modify_account_key
     @name=User.find(params[:id]).name
   end
@@ -85,9 +109,7 @@ class UserController < ApplicationController
     end
   end
 
-  def login_success
-    @user = session[:user]
-  end
+
 
   def judge_input_legal
     session[:add_or_register]="register"
@@ -193,22 +215,7 @@ class UserController < ApplicationController
     end
   end
 
-  def handle_login
-    username = params[:user][:name]
-    password = params[:user][:password]
-    @user = User.find_by_name_and_password username, password
-    if !@user.nil?
-      if @user.name!="admin"
-        redirect_to '/user/login_success' #redirect_to  user_login_success_path
-        session[:user] = @user
-      else
-        redirect_to '/user/manage_index'
-      end
-    else
-      flash.now[:notice]= '用户名或者密码不正确'
-      render 'login'
-    end
-  end
+
 
   private
   def user_params
